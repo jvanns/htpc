4afee4d075e94b723dcd16dbdb3fa3259eada8ff
Jim Vanns <james.vanns@gmail.com>
2014-07-30 21:33:10 +0100

    embed-album-art: Rename get-album-art.sh and actually embed the images
    Attempt to embed the image file data into every MP3 file in the album

--- embed-album-art.sh
+++ embed-album-art.sh
@@ -3,7 +3,10 @@
 # This simple script will fetch the cover art for the album information
 # provided on the command line. It will then download that cover image,
 # and place it into the temporary directory. The term "album information" is
-# really the album-named relative path of the current directory.
+# really the album-named relative path of the current directory. 
+#
+# The second stage is to embed the binary data of this image file into
+# the ID3v2 APIC frame, into every file beneath the given directory.
 #
 
 if [ "x$1" = "x" ]
@@ -12,6 +15,7 @@ then
 	exit 1
 fi
 
+IMG="${TMP}/album-art.jpg"
 PAGE='http://www.albumart.org/index.php'
 ESCAPED="`perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "${1// /+}"`"
 URL="${PAGE}?searchkey=$ESCAPED&itempage=1&newsearch=1&searchindex=Music"
@@ -25,6 +29,10 @@ COVERURL=`wget -qO - "$URL" | $XMLCMD \
 'string(//a[@title="View larger image" and starts-with(@href, "'$AMAZON'")]/@href)' - 2> /dev/null`
 
 echo "Cover URL: [$COVERURL]"
-wget -qO - "$COVERURL" 1> "${TMP}/album-art.jpg"
-[ $? -eq 0 ] && [ -s "${TMP}/album-art.jpg" ] && exit 0
+wget -qO - "$COVERURL" 1> "$IMG"
+[ $? -ne 0 ] && [ ! -s "$IMG" ] && exit 1
+
+echo "Embedding ... [`stat -c %s $IMG`]"
+find "$1" -type f -name '*.mp3' \
+| xargs -- eye3d -2 --add-image="${IMG}:FRONT_COVER"
 
