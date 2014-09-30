e6f7819326d14cd6946b1911cd905a64f5f4af82
Jim Vanns <james.vanns@gmail.com>
2014-09-30 20:51:01 +0100

    makemkv-key-update: Bug fix - remove single quotes around URL

--- makemkv-key-update.sh
+++ makemkv-key-update.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 URL='http://www.makemkv.com/forum2/viewtopic.php?f=5&t=1053'
-KEY="`lynx -source '$URL' | grep 'codecontent">' | cut -d \> -f11 | sed 's/<\/div$//'`"
+KEY="`lynx -source $URL | grep 'codecontent">' | cut -d \> -f11 | sed 's/<\/div$//'`"
 
 echo "app_Key = \"$KEY\""
