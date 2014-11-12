05c3f4f8dacb4eca8b0005bb8becc170acfc8ae3
Jim Vanns <james.vanns@gmail.com>
2014-11-12 19:13:21 +0000

    itunes-migrator: Augment script to begin processing files
    This will launch itself in zygote mode to feed itself with
    input files from the bootstrap 'copy' mode invoked by the
    user.

--- itunes-migrator.sh
+++ itunes-migrator.sh
@@ -1,8 +1,48 @@
-#!/bin/sh
+#!/bin/bash
 
 set -eu
 
-cat <<EOF | mediainfo --Inform='file:///dev/stdin' "$1"
-General;%Encoded_Library%,%CompleteName%,
-Audio;%Format%,%Language%,%Channels%,%BitDepth%,%BitRate%
+SELF="$0"
+MODE="${1:-}"
+
+copy_files() {
+	grep -iv '^itunes.*|/'
+}
+
+identify_files() {
+	for f in "$@"
+	do
+cat <<EOF | mediainfo --Inform='file:///dev/stdin' "$f"
+General;%Encoded_Library%|%CompleteName%|
+Audio;%Format%|%Language%|%Channels%|%BitDepth%|%BitRate%
 EOF
+	done
+}
+
+bootstrap_migration() {
+	local l=64
+	local p=`getconf _NPROCESSORS_ONLN`
+
+	find "$1" -type f \
+		\! -name 'XXX' \
+		-print0 \
+	| xargs -0 -P$p -n$l -- $SELF zygote
+}
+
+usage() {
+	echo "`basename "$SELF"`: copy <from directory> <to directory>"
+	exit 1
+}
+
+if [ "$MODE" = 'copy' ]; then
+	shift
+	bootstrap_migration "$1" "$2"
+elif [ "$MODE" = 'zygote' ]; then
+	shift
+	identify_files "$@" | copy_files
+elif [ "x${MODE}" = 'x' ]; then
+	usage
+else
+	echo "Unknown mode '$MODE'" >&2
+	usage
+fi
