67dfda61e772cde57aea07cedf31b0bdc61300c5
Jim Vanns <jim.vanns@framestore.com>
2014-11-13 12:13:58 +0000

    itunes-migrator: Test for both Encoded_Library and Encoded_Application
    Either of these could state iTunes was used to encode the file.
    Remove the use of a HERE document instance per file - an utter
    waste of processing. Build the file once when in 'copy' mode
    then refer to it for each zygote.

--- itunes-migrator.sh
+++ itunes-migrator.sh
@@ -4,27 +4,36 @@ set -eu
 
 SELF="$0"
 MODE="${1:-}"
+INFO_FILE="/tmp/`basename $SELF`.inf"
 
 format_files() {
-	while IFS=$'|' read encoder file album artist title format bdepth brate
+	while IFS=$'|' read enclib encapp file album artist title format bdep brat
 	do
+		if [[ "$enclib" =~ ^itunes ]] || [[ "$encapp" =~ ^itunes ]]; then
+			continue
+		fi
 		echo -e "$file\t$album"
-	done | sort -t $'\t' -k2
+	done
 }
 
 identify_files() {
+	shopt -s nocasematch
+
 	for f in "$@"
 	do
-cat <<EOF | mediainfo --Inform='file:///dev/stdin' "$f"
-General;%Encoded_Library%|%CompleteName%|%Album%|%Performer%|%Track name%|
-Audio;%Format%|%BitDepth%|%BitRate%
-EOF
-	done | grep -iv '^itunes.*|/' | format_files
+		mediainfo --Inform="file://$INFO_FILE" "$f"
+	done | format_files
 }
 
 bootstrap_migration() {
 	local l=64
 	local p=`getconf _NPROCESSORS_ONLN`
+
+	echo -ne 'General;%Encoded_Library%|%Encoded_Application%|' > "$INFO_FILE"
+	echo -e  '%CompleteName%|%Album%|%Performer%|%Track%|'      >> "$INFO_FILE"
+	echo -e  'Audio;%Format%|%BitDepth%|%BitRate%'              >> "$INFO_FILE"
+
+	trap "rm -f $INFO_FILE" EXIT
 
 	find "$1" -type f \
 		\! -name '*.mp4' \
